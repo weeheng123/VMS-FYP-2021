551f1056db55ab1b0a87a6d32fc28eec23094b6e
Wee Heng Goh <wee.heng@ciscosecurity.com>
2020-12-30 16:45:07 +0800

    4th heroku db commit

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ const { MemoryStore } = require('express-session');
 
 var port = process.env.PORT || 5000;
 
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -40,3 +49,4 @@ app.use('/',ancRoutes.ancRoutes);
 var listener = app.listen(port, function(){
     console.log('Listening on port ' + listener.address().port); //Listening on port 5000
 });
+
