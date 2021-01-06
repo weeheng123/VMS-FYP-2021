df43fd6e5417d4332022781857b477c5b0f085e9
Wee Heng Goh <wee.heng@ciscosecurity.com>
2021-01-06 16:59:52 +0800

    5th heroku commit 3rd January

---
+++ controllers/qr_controller.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var path = require('path');
+var session = require('express-session');
+var models = require('../models');
+var Sequelize = require('sequelize');
+var kenx = require('knex');
+const bcrypt = require('bcrypt');
+const { Client } = require('pg');
+const user = require('../models/user');
+const methodOverride = require('method-override');
+const { Result } = require('express-validator');
+
+
+var qrRoutes = express();
+//Change from express.Router() to express();
+
+qrRoutes.use(bodyParser.json()) // for parsing application/json
+qrRoutes.use(bodyParser.urlencoded({ extended: true }))
+qrRoutes.use(methodOverride('_method'));
+
+const client = kenx({
+    client: "pg",
+    connection: {
+    connectionString: process.env.DATABASE_URL
+    },
+    dialectOptions: {
+        ssl: {
+          require: true,
+          // Ref.: https://github.com/brianc/node-postgres/issues/2009
+          rejectUnauthorized: false,
+        },
+      }
+});
+
+qrRoutes.post('/app/qr', function(req,res){
+    models.qrentries.create({
+        name: req.body.name,
+        ic: req.body.oriic,
+        address: req.body.address,
+        oriname: req.body.oriname,
+        oriic: req.body.oriic,
+        oriaddress: req.body.oriaddress,
+        qrimage: req.body.qrimage,
+        checkin: req.body.checkin,
+        checkout: req.body.checkout
+    }).then(function(){
+        res.status(200);
+    });
+});
+
+
+module.exports = {"qrRoutes" : qrRoutes};
