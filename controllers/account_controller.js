9039a9b8a46f409ed16363fc5fe851e1093642cd
Wee Heng Goh <wee.heng@ciscosecurity.com>
2021-01-07 20:53:09 +0800

    29th heroku commit 7th January

--- controllers/account_controller.js
+++ controllers/account_controller.js
@@ -148,10 +148,10 @@ accountRoutes.post('/app/login', function(req,res){
         if(users.length > 0){
             let user = users[0];
             let passwordHash = user.password;
-            const userArray ;
+            const userArray = [];
             if(bcrypt.compareSync(req.body.password, passwordHash)){
                 client.select("role","username").from("users").where( { username: req.body.username }).then(data =>{
-                    res.status(200).send(JSON.stringify({users:data})
+                    res.status(200).send(JSON.stringify({users:data}))
                     console.log({users:data});
                 })
                 // console.log(rolequery.role);
