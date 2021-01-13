fe54e733553861a147768fbe594ffbc827a23d3d
Wee Heng Goh <wee.heng@ciscosecurity.com>
2021-01-13 15:55:03 +0800

    1st heroku commit 13th January

--- controllers/qr_controller.js
+++ controllers/qr_controller.js
@@ -53,5 +53,16 @@ qrRoutes.post('/app/qr', function(req,res){
     }
     );
 
+qrRoutes.post('/app/qrstatus', function(req,res){
+    if(req.body.ic){
+    client.select("checkin").from("qrentries").where({ic: req.body.ic}).then(data =>{
+        res.status(200).send(JSON.stringify({qrstatus:data}));
+        console.log(data)
+    })}
+    else{
+        res.status(404).send();
+    }
+})
+
 
 module.exports = {"qrRoutes" : qrRoutes};
