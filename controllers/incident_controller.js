35cb261dd401e4e62f182b625f0c4243163e35e4
Wee Heng Goh <wee.heng@ciscosecurity.com>
2021-01-20 15:31:15 +0800

    7th heroku commit 20th January

--- controllers/incident_controller.js
+++ controllers/incident_controller.js
@@ -69,7 +69,7 @@ inRoutes.put('/app/incident/status', function(req,res){
     })
 })
 
-inRoutes.post('/app/incident/get', function(req,res){
+inRoutes.get('/app/incident/get', function(req,res){
     client.select("*").where({name: req.body.name}).from("incidents").then(data =>{
         res.status(200).send(JSON.stringify({incident:data}));
         console.log({incident:data})
